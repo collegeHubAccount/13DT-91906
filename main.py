57905ae4e95b510dcc40c4290e156f2e64146239
collegeHubAccount <100169715+collegeHubAccount@users.noreply.github.com>
2023-07-27 10:33:14 +1200

    Added file reading
    -Added bands.txt witch contains set list of resistor bands
    -Added file reading of bands.txt as a list
    -Added a system to go through all bands one at a time (pre system for quiz)

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import resistor
 import random
-value = resistor.res(1021)
-print(value)
+# init
+exclude = []
+with open(f"bands.txt") as f:
+    bands = f.readlines()
+for line in range(len(bands)):
+    bands[line] = bands[line][0:-1]
+
+# main
+print(bands)
+while 1:
+    if bands != []:
+        band = random.randint(0,(len(bands)-1))
+        value = resistor.bandToValue(bands[band])
+        bands.pop(band)
+        print(value)
+    else:
+        break
+print("program complete")
--- resistor.py
+++ resistor.py
@@ -1,7 +1,4 @@
-import random
-
-class res(object):
-    def __new__(self, order) -> None:
-        self.multi = 10**int(str(order)[-1])
-        self.ans = int(str(order)[0:-1])*self.multi
-        return int(self.ans)
+def bandToValue(order) -> None:
+    multi = 10**int(str(order)[-1])
+    ans = int(str(order)[0:-1])*multi
+    return int(ans)
