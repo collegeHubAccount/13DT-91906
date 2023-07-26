fd3578070a7fba86b32cf72fc2eb87a41280dff2
collegeHubAccount <100169715+collegeHubAccount@users.noreply.github.com>
2023-07-27 11:17:55 +1200

    Add files via upload
    -Updated main.py to act as a quiz with text based gui
        -Added scoring system for quiz

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import random
 exclude = []
 with open(f"bands.txt") as f:
     bands = f.readlines()
+total_questions = len(bands)
 for line in range(len(bands)):
     bands[line] = bands[line][0:-1]
 
 # main
-print(bands)
+# print(bands)
+score = 0
 while 1:
     if bands != []:
         band = random.randint(0,(len(bands)-1))
         value = resistor.bandToValue(bands[band])
+        print(bands[band])
+        # print(f"debug {value}")
         bands.pop(band)
-        print(value)
+        ans = input("yo gimme a value for this ting: ")
+        if int(ans) == value:
+            score += 1
     else:
         break
-print("program complete")
+print(f'you got {score} out of {total_questions}')
