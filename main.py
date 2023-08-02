879572fa56f13a3470909116cf6a9756f51f5ae1
collegeHubAccount <100169715+collegeHubAccount@users.noreply.github.com>
2023-08-02 14:25:23 +1200

    Made gui version **-Does not include quiz**
    A gui system using tkinter for displaying colour bands does not include the code for asking questions.

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
-import resistor
 import random
-# init
-exclude = []
-with open(f"bands.txt") as f:
-    bands = f.readlines()
-total_questions = len(bands)
-for line in range(len(bands)):
-    bands[line] = bands[line][0:-1]
+import application
+import tkinter as tk
 
-# main
-# print(bands)
-score = 0
-while 1:
-    if bands != []:
-        band = random.randint(0,(len(bands)-1))
-        value = resistor.bandToValue(bands[band])
-        print(bands[band])
-        # print(f"debug {value}")
-        bands.pop(band)
-        ans = input("yo gimme a value for this ting: ")
-        if int(ans) == value:
-            score += 1
-    else:
-        break
-print(f'you got {score} out of {total_questions}')
+# functions
+def bandToValue(order) -> None:
+    multi = 10**int(str(order)[-1])
+    ans = int(str(order)[0:-1])*multi
+    return int(ans)
+
+def importer(file):
+    with open(f"{file}") as f:
+        bands = f.readlines()
+    for line in range(len(bands)):
+        bands[line] = bands[line][0:-1]
+    total_questions = len(bands)
+    data = [bands, total_questions]
+    return data
+
+
+if __name__ == "__main__":
+    # init
+    exclude = []
+    bands = importer("bands.txt")
+    total_questions = bands[1]
+    bands = bands[0]
+    score = 0
+
+    app = application.App()
+    
+
+    # prog loop
+    # while 1:
+    #     if bands != []:
+    #         band = random.randint(0,(len(bands)-1))
+    #         value = bandToValue(bands[band])
+    #         print(bands[band])
+    #         # print(f"debug {value}")
+    #         bands.pop(band)
+    #         ans = input("yo gimme a value for this ting: ")
+    #         if int(ans) == value:
+    #             print("correct!")
+    #             score += 1
+    #         else:
+    #             print(f"nice try the correct values was {value}")
+    #     else:
+    #         break
+    # # final score display
+    # print(f'you got {score} out of {total_questions}')
+    app.draw(app.canvas, app.x1, app.y1, app.x2, app.y2, 
+                 "red", "green", "purple", "yellow")
+    app.ready(app.canvas)
+    app.mainloop()
