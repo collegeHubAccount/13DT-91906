d780ec91fabfab52fbc33d2d3c3ada8a1c5f3b63
collegeHubAccount <100169715+collegeHubAccount@users.noreply.github.com>
2023-08-17 13:30:21 +1200

    Pep8 compliant, added accessibility feature
    -Colours are listed as words for every resistor

--- application.py
+++ application.py
@@ -13,31 +13,37 @@ class App(tk.Tk):
         self.canvas_h = 480
         self.geometry(f"{self.canvas_w}x{self.canvas_h}+240+100")
         self.canvas_h *= 0.3
-        self.displace = -140/2
-        self.x1 = (self.canvas_w*0.5)+self.displace
-        self.y1 = (self.canvas_h*0.5)
-        self.x2 = (self.canvas_w*0.5)+self.displace+20
-        self.y2 = (self.canvas_h*0.5)-40
+        self.displace = -140 / 2
+        self.x1 = (self.canvas_w * 0.5) + self.displace
+        self.y1 = (self.canvas_h * 0.5)
+        self.x2 = (self.canvas_w * 0.5) + self.displace + 20
+        self.y2 = (self.canvas_h * 0.5) - 40
 
         # objects
         self.canvas = tk.Canvas(self, width=self.canvas_w,
                                 height=self.canvas_h)
         self.entry = tk.Entry(self, width=40)
+        self.lbl = tk.Label(self, text='Enter the resistor value')
+        self.display_txt = tk.Label(self, text='')
+        self.ques = tk.Label(self, text='')
 
 
     def draw(self, canvas, x1, y1, x2, y2, 
-             colour1, colour2, colour3, colour4, colour5 = "brown"):
-        canvas.create_rectangle(x1,y1,x2,y2,fill=colour1)
-        canvas.create_rectangle(x1+30,y1,x2+30,y2,fill=colour2)
-        canvas.create_rectangle(x1+60,y1,x2+60,y2,fill=colour3)
-        canvas.create_rectangle(x1+90,y1,x2+90,y2,fill=colour4)
-        canvas.create_rectangle(x1+120,y1,x2+120,y2,fill=colour5)
+             colour1, colour2, colour3, colour4, colour5="brown"):
+        canvas.create_rectangle(x1, y1, x2, y2, fill=colour1)
+        canvas.create_rectangle(x1 + 30, y1, x2 + 30, y2, fill=colour2)
+        canvas.create_rectangle(x1 + 60, y1, x2 + 60, y2, fill=colour3)
+        canvas.create_rectangle(x1 + 90, y1, x2 + 90, y2, fill=colour4)
+        canvas.create_rectangle(x1 + 120, y1, x2 + 120, y2, fill=colour5)
     
     # def confirm(self, event=None):
     #     self.ans = self.entry.get()
     #     print(self.ans)
 
+
     def ready(self):
-        self.canvas.pack(side=tk.TOP)
+        self.canvas.pack()
+        self.display_txt.pack()
         self.entry.pack()
-        # self.bind('<Return>', self.confirm)
+        self.lbl.pack()
+        self.ques.pack()
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import application
 import tkinter as tk
 
 # functions
+
+
 def bandToValue(order) -> None:
-    multi = 10**int(str(order)[-1])
-    ans = int(str(order)[0:-1])*multi
+    if str(order)[-1] == 'a':
+        multi = 10 ** -1
+    elif str(order)[-1] == 'b':
+        multi = 10 ** -2
+    else:
+        multi = 10 ** int(str(order)[-1])
+    ans = int(str(order)[0:-1]) * multi
     return int(ans)
 
 
@@ -13,46 +20,78 @@ def importer(file):
     with open(f"{file}") as f:
         bands = f.readlines()
     for line in range(len(bands)):
-        bands[line] = bands[line][0:-1]
+        bands[line] = bands[line][:4]
     total_questions = len(bands)
     data = [bands, total_questions]
     return data
+# Make quesiton not go over limit (10/9)
+# add some silver color code multiplyers
 
 
 def display():
     global bands, value, run
-    colours = ['black', 'brown', 'red', 'orange', 'yellow', 
-               'green', 'blue', 'violet', 'grey', 'white']
+    colours = ['black', '#5e3d05', 'red', '#ff8400', 'yellow', 
+               'green', 'blue', 'violet', 'grey', 'white', 
+               '#dbac34', '#a5a9b4']
+    colours_writen = ['Black', 'Brown', 'Red', 'Orange', 
+                      'Yellow', 'Green', 'Blue', 'Purple', 
+                      'Grey', 'White', 'Gold', 'Silver']
     if bands != []:
-        band = random.randint(0,(len(bands)-1))
+        band = random.randint(0, (len(bands) - 1))
         value = bandToValue(bands[band])
-        app.draw(app.canvas, app.x1, app.y1, app.x2, app.y2, 
-                    colours[int(str(bands[band])[0])], 
-                    colours[int(str(bands[band])[1])], 
-                    colours[int(str(bands[band])[2])], 
-                    colours[int(str(bands[band])[3])])
+        alt = False
+        for i in range(4):
+            if bands[band][i] == 'a':
+                alt = 'a'
+            elif bands[band][i] == 'b':
+                alt = 'b'
+        first = int(str(bands[band])[0])
+        second = int(str(bands[band])[1])
+        third = int(str(bands[band])[2])
+        if not alt:
+            fourth = int(str(bands[band])[3])
+        elif alt == 'a':
+            fourth = 10
+        elif alt == 'b':
+            fourth = 11
+        app.draw(app.canvas, app.x1, app.y1, app.x2, app.y2, colours[first], 
+                 colours[second], colours[third], colours[fourth])
+        app.display_txt.config(text=f'\
+{colours_writen[first]} {colours_writen[second]}\
+ {colours_writen[third]} {colours_writen[fourth]}')
         bands.pop(band)
     else:
         run = False
 
 
 def confirm(event=None):
-    global score
-    if run:
-        app.ans = app.entry.get()
+    global score, question
+    app.ans = app.entry.get()
+    if run and app.ans != '' and not question > total_questions:
+        if question != total_questions:
+            question += 1
+            app.ques.config(text=f'Question {question}/{total_questions}')
+        else:
+            app.ques.config(text='Press enter to view your final score')
         if int(app.ans) == value:
-            print("correct!")
+            app.lbl.config(text=f'Correct it was {value}')
             score += 1
         else:
-            print(f"nice try the correct values was {value}")
+            app.lbl.config(text=f'nice try the correct values was {value}')
         # final score display
         display()
-    else:
-        print(f"total score: {score}")
+    elif not run and question == total_questions:
+        app.lbl.config(text=f'Total score: {score}/{total_questions}')
+        if score >= total_questions//2:
+            app.ques.config(text='Well done thats a passing grade!')
+        else:
+            app.ques.config(text="It looks like you still need \
+some practise, don\'t worry keep trying")
 
 
 if __name__ == "__main__":
     # init
+    question = 1
     exclude = []
     bands = importer("bands.txt")
     total_questions = bands[1]
@@ -61,6 +100,7 @@ if __name__ == "__main__":
     run = True
     app = application.App()
     app.ready()
+    app.ques.config(text=f'Question {question}/{total_questions}')
     app.bind('<Return>', confirm)
     display()
     app.mainloop()
